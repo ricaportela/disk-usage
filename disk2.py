37a6656624bf57a0396c348fae0cf5db262b2667
ricaportela <ricaportela@gmail.com>
2017-01-22 20:51:25 -0200

    update

--- disk2.py
+++ disk2.py
@@ -12,8 +12,9 @@ def get_mount_point():
     with open('out.json', 'w') as outfile:
         for item in f_file:
             linha = item.split()
-            jsondata = json.dumps(linha, separators=(',', ': '), indent=4)
-            outfile.write(jsondata)
+            if linha[2:10] <> 'Filesystem':
+                jsondata = json.dumps(linha, separators=(',', ': '), indent=4)
+                outfile.write(jsondata)
 
 if __name__ == ("__main__"):
     get_mount_point()
