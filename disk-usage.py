c240b79c9b3b37695f2defcbf60dfe9ad63404cc
Ricardo <ricpds@oc8422381675.ibm.com>
2017-01-22 08:20:31 -0200

    Update Project

--- disk-usage.py
+++ disk-usage.py
@@ -1,36 +1,32 @@
 import os
 import sys
 import json
-# Sist. Arq.                           Tam. Usado Disp. Uso% Montado em
-# /dev/mapper/vg_oc3060845136-lv_root  285G   35G  236G  13% /
-
-jsonData = { "Sist. Arq": "/dev/mapper/vg_oc3060845136-lv_root",
-              "Tam": "285G",
-              "Usado": "35G",
-              "Uso": "236G",
-              "Montado": "/"
-          }
 
 
 def get_mount_point():
+    """ Retrieve Mount points """
     # print subprocess.call([df --output=avail -h "$PWD" | sed '1d;s/[^0-9]//g'])
     print os.system('df -h > text.txt')
 
+
 def get_from_text():
-    with open('text.txt', 'r') as f_file:
-        for line in f_file:
-            dicionario = line.split()
-            print "Sist. Arq. ", dicionario[0]
-            print "Tam        ", dicionario[1]
-            print "Usado      ", dicionario[2]
-            print "Disponivel ", dicionario[3]
-            print "Em Uso     ", dicionario[4]
-            print "Montado em ", dicionario[5]
+    """ Update Json file with Text information """
+    
+    with open('out.json','w') as outfile:
+        with open('text.txt', 'r') as f_file:
+            for line in f_file:
+                dicionario = line.split()
+                jsondata = json.dumps({"Sist. Arq": dicionario[0],
+                "Tamanho   ": dicionario[1],
+                "Usado     ": dicionario[2],
+                "Disponivel": dicionario[3],
+                "Uso       ": dicionario[4],
+                "Montado em": dicionario[5]
+                }, separators=(',', ': '))
+                               
+                outfile.write(jsondata)
 
-            #with open('out.json', 'w') as j_file:
-            #     json.dump(jsonData, f_file)
-
-
+        
 if __name__ == ("__main__"):
     get_mount_point()
     get_from_text()
--- teste_json.py
+++ teste_json.py
@@ -3,11 +3,12 @@ import json
 # Sist. Arq.                           Tam. Usado Disp. Uso% Montado em
 # /dev/mapper/vg_oc3060845136-lv_root  285G   35G  236G  13% /
 
-jsonData = { "Sist. Arq": "/dev/mapper/vg_oc3060845136-lv_root",
-              "Tam": "285G",
-              "Usado": "35G",
-              "Uso": "236G",
-              "Montado": "/"
+jsonData = {  "Sist. Arq ": "/dev/mapper/vg_oc3060845136-lv_root",
+              "Tamanho   ": "285G",
+              "Usado     ": "35G",
+              "Disponivel": "236G",
+              "Uso       ": "13%",
+              "Montado em": "/"
           }
 
 
