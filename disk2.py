53a12a9925bcd5c11676727e20cc2cea9a126076
Ricardo Portela <ricaportela@gmail.com>
2017-01-22 22:41:24 -0200

    Update disk2.py
    Update from Jan Palcha (Facebook)

--- disk2.py
+++ disk2.py
@@ -1,20 +1,23 @@
-""" Retrieve data from command line """
+""" Retrieve Data from Command line """
+
+from collections import namedtuple
 import subprocess
 import json
 
+FData = namedtuple("FData", "filesystem size used avail perc_used mounted_on")
 
 def get_mount_point():
     """ Retrieve Mount points """
-    jsondata = {}
     output = subprocess.check_output(['df', '-h'])
     output = output.decode('utf-8')
     f_file = output.split('\n')
+    usage_list = []
     with open('out.json', 'w') as outfile:
-        for item in f_file:
-            linha = item.split()
-            if linha[2:10] <> 'Filesystem':
-                jsondata = json.dumps(linha, separators=(',', ': '), indent=4)
-                outfile.write(jsondata)
+        for item in f_file[1:-1]:
+            line = item.split()
+            usage_list.append(FData(*line)._asdict())
+            print(usage_list)
+        outfile.write(json.dumps(usage_list))
 
 if __name__ == ("__main__"):
     get_mount_point()
