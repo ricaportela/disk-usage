3898999fd15faf93860376afe0a3cc5389a2aee5
ricaportela <ricaportela@gmail.com>
2017-01-23 22:37:14 -0200

    Create REad file Json

--- read_json.py
+++ read_json.py
@@ -4,4 +4,14 @@ import json
 
 def read_json():
     """ Read values from Json File """
-    with open('out.json','e') as f_json:
+    data = []
+    with open('out.json') as f_json:
+        data = json.load(f_json)
+        for p in data[1:1]:
+            print p['size'], ' ', p['used'], ' ', p['avail']
+
+
+if __name__ == ("__main__"):
+    read_json()
+
+
