ab50af29060910154255680fa69670dbd9bb2825
ricaportela <ricaportela@gmail.com>
2017-01-23 22:16:31 -0200

    Update files

--- disk1.py
+++ disk1.py
@@ -1,14 +1,22 @@
 from matplotlib import pyplot as plt
 
 
-# Data to plot
-labels = 'Python', 'C++', 'Ruby', 'Java'
-sizes = [215, 130, 245, 210]
-colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
-explode = (0.1, 0, 0, 0)  # explode 1st slice
- 
-# Plot
-plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
- 
-plt.axis('equal')
-plt.show()
+def plot_data():
+    """ Draw Pie Chart with values from Json File """
+    # Data to plot
+    labels = 'Used', 'Available', 'Use%'
+    sizes = [215, 210, 134]
+    colors = ['gold', 'yellowgreen', 'lightcoral']
+    explode = (0.1, 0, 0)  #  explode 1st slice
+    plt.title('Filesystem'+'\n'+'Size of '+'Mounted on ')
+    # Plot
+    plt.pie(sizes, explode=explode, labels=labels, \
+                   colors=colors, autopct='%1.1f%%', \
+                   shadow=True, startangle=140)
+
+    plt.axis('equal')
+    plt.show()
+
+if __name__ == ("__main__"):
+    plot_data()
+    
---
+++ read_json.py
@@ -0,0 +1,7 @@
+""" Module Read json File """
+import json
+
+
+def read_json():
+    """ Read values from Json File """
+    with open('out.json','e') as f_json:
